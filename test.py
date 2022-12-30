23171669d6b989ffb90a80b306aaf08def755450
Andres Scheffer <92527833+andresbbx@users.noreply.github.com>
2022-12-30 14:11:42 +0000

    mqtt client

--- test.py
+++ test.py
@@ -1,13 +1,44 @@
-from paho.mqtt import client as mqtt
+import random
 
-# Crear un cliente MQTT
-client = mqtt.Client()
+from paho.mqtt import client as mqtt_client
 
-# Establecer la conexión con el broker MQTT
-client.connect("broker.emqx.io", 1883, 60)
 
-# Publicar el mensaje
-client.publish("artux/test", "Hola, mundo!")
+broker = 'broker.emqx.io'
+port = 1883
+topic = "artux/mqtt"
+# generate client ID with pub prefix randomly
+client_id = f'python-mqtt-{random.randint(0, 100)}'
+username = 'emqx'
+password = 'public'
 
-# Desconectar el cliente
-client.disconnect()
+
+def connect_mqtt() -> mqtt_client:
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt_client.Client(client_id)
+    client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+
+def subscribe(client: mqtt_client):
+    def on_message(client, userdata, msg):
+        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+
+    client.subscribe(topic)
+    client.on_message = on_message
+
+
+def run():
+    client = connect_mqtt()
+    subscribe(client)
+    client.loop_forever()
+
+
+if __name__ == '__main__':
+    run()
